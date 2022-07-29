6695b6c3162d8400c274332ce429460514c39c37
Bantealem <bantem2021@gmail.com>
2022-07-30 01:00:54 +0300

    update changes

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-![](https://img.shields.io/badge/Microverse-blueviolet)
 
-Here is a link to my loom video [Project description video](https://www.loom.com/share/09d096e5d43442dab33e23fe8e41cae4)
+
+Here is a link to my loom video [Project description video]()
 
 # First Capstone Project: Responsive Restaurant Website
 
-![screenshot](./images/readme-img.png)
+![screenshot](./images/readme.jpg)
 
-> This is my capstone project built with HTML-CSS-BOOTSTRAP-JAVASCRIPT. My love for restaurants made me develop this project along that line. The website is about a popular restuarant called Foodland restaurant. It features a list of dishes, an about page and partner sections. Please have a look at the live demo and let me know what you think 😊.
+> This is my capstone project built with HTML-CSS-BOOTSTRAP-JAVASCRIPT. My love for restaurants made me develop this project along that line. The website is about a popular restuarant called YOD Abbisinia restaurant. It features a list of dishes, an about page and partner sections. Please have a look at the live demo and let me know what you think 😊.
 
 ## Built With
 
@@ -28,8 +28,8 @@ you can clone the repository by using the command below and run the "npm install
 ### Setup
 
 ```cmd
-git clone git@github.com:Elijahdre/Capstone-one.git
-cd ./Capstone-one
+git clone git@github.com:Bantealem/Capstone1.git
+cd ./Capstone1
 npm install
 npm run build
 npm start
@@ -41,7 +41,6 @@ If you are non a technical person you can download the zip file.
 - Choose the option 'Download Zip' and wait until it download.
 - Then you need to decompress it.
 - You will need to have Zip compress file software installed in your computer. If you don't have it you can download it from here
-  [`https://www.7-zip.org/download.html`](https://www.7-zip.org/download.html)
 - Do double click on the Capstone-1.zip and choose a folder where to place all the decompressed files.
 - Once you are done decompressing, you can open the created folder.
 - In it you can double click on the index.html file and choose a browser to open it (For example google Chrome Browser).
---
+++ js/cards.js
@@ -0,0 +1,102 @@
+const ProgramDetails = [
+  {
+    title: 'Food',
+    description:
+      'A typical dish consists of injera accompanied by a spicy stew, which frequently includes beef, lamb, vegetables and various types of legumes, such as lentils.',
+    icon: './images/lecture.svg',
+  },
+  {
+    title: 'Music',
+    description:
+      'The music of the Ethiopian Highlands uses a fundamental modal system called qenet, of which there are four main modes: tezeta, bati, ambassel, and anchihoy',
+    icon: './images/workshop.svg',
+  },
+  {
+    title: 'Theater',
+    description:
+      ' theater in Ethiopia has become a very popular art type mainly in the capital Addis Ababa. theater in Ethiopia has become a very popular art type mainly in the capital Addis Ababa..',
+    icon: './images/forum.svg',
+  },
+  {
+    title: 'Dance',
+    description:
+      'Eskista (Amharic: እስክስታ) is a traditional Ethiopian cultural dance from the Amhara ethnic group performed by men, women, and children.',
+    icon: './images/ignite.svg',
+  },
+  {
+    title: 'Drinks',
+    description:
+      'Tella, Borde, Shamita, Korefe, Cheka, Tej, Ogol, Booka, and Keribo are among the popular alcoholic beverages in the country.',
+    icon: './images/cc.svg',
+  },
+];
+
+ProgramDetails.forEach((program) => {
+  const card = `<div class='content-card'>
+    <img src='${program.icon}' alt='${program.title}'>
+    <h4 class='content-title'>${program.title}</h4>
+    <p>
+        ${program.description}
+    </p>
+  </div>`;
+  document.querySelector('.content').innerHTML += card;
+});
+
+const Speakers = [
+  {
+    image: './images/food13.jpg',
+    name: 'Tibs',
+    designation: 'Made from meat',
+    description:
+      'It is made from meat.',
+  },
+  {
+    image: './images/food21.jpg',
+    name: 'Tegabino',
+    designation: 'Made from a powder',
+    description:
+      'Made from a bee powder.',
+  },
+  {
+    image: './images/food15.jpg',
+    name: 'Shiro',
+    designation: 'Made from bee powder',
+    description:
+      't Most Ethiopians daily food',
+  },
+  {
+    image: './images/food18.png',
+    name: 'Kitfo',
+    designation: 'Made from meat',
+    description:
+      'It is Gurage ethinic group traditional food.',
+  },
+  {
+    image: './images/food20.jpg',
+    name: 'Doro wot',
+    designation: 'Made from chicken',
+    description:
+      'It is Amhara Ethinic group traditional food.',
+  },
+  {
+    image: './images/food19.jpg',
+    name: 'Key wot',
+    designation: 'meat',
+    description:
+      'It is normal food made from meat',
+  },
+];
+
+Speakers.forEach((speaker) => {
+  const card = `<div class='speaker-card'>
+    <div class='speaker-img'>
+      <img src='${speaker.image}' alt='speaker one' />
+    </div>
+    <div class='speaker-details'>
+      <h3>${speaker.name}</h3>
+      <em>${speaker.designation}</em>
+      <p>${speaker.description}</p>
+    </div>
+  </div>`;
+  document.querySelector('.speaker-list').innerHTML += card;
+});
